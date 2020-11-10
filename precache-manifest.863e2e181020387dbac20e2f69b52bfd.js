41217b1307e5552aa9c663a766ad8ae75a9780eb
thanigaivel569 <thanigaiveltizen@gmail.com>
2020-11-10 14:36:38 +0530

    Updates

---
+++ precache-manifest.863e2e181020387dbac20e2f69b52bfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/QR_Scan_POC/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "98bcebc74dead4edc60d",
+    "url": "/QR_Scan_POC/static/js/runtime~main.98bcebc7.js"
+  },
+  {
+    "revision": "54469bd7c2ec3152fe96",
+    "url": "/QR_Scan_POC/static/js/main.54469bd7.chunk.js"
+  },
+  {
+    "revision": "9eab4fe570356fc88392",
+    "url": "/QR_Scan_POC/static/js/1.9eab4fe5.chunk.js"
+  },
+  {
+    "revision": "54469bd7c2ec3152fe96",
+    "url": "/QR_Scan_POC/static/css/main.b375beb7.chunk.css"
+  },
+  {
+    "revision": "209f83f5b44d1dda6e5234e9ce3f8a72",
+    "url": "/QR_Scan_POC/index.html"
+  }
+];
