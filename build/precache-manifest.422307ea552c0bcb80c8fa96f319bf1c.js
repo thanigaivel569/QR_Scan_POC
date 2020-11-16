3f289f54ae133e2fbc9d11d233b2510149244ee7
thanigaivel569 <thanigaiveltizen@gmail.com>
2020-11-16 23:31:25 +0530

    QRScan

---
+++ build/precache-manifest.422307ea552c0bcb80c8fa96f319bf1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98bcebc74dead4edc60d",
+    "url": "/QR_Scan_POC/static/js/runtime~main.98bcebc7.js"
+  },
+  {
+    "revision": "3ead42312d678a274d41",
+    "url": "/QR_Scan_POC/static/js/main.3ead4231.chunk.js"
+  },
+  {
+    "revision": "ab7fb3c24c3cd6401663",
+    "url": "/QR_Scan_POC/static/js/1.ab7fb3c2.chunk.js"
+  },
+  {
+    "revision": "3ead42312d678a274d41",
+    "url": "/QR_Scan_POC/static/css/main.156eda13.chunk.css"
+  },
+  {
+    "revision": "741aa4e41661c14ec2c5dc541b287bc0",
+    "url": "/QR_Scan_POC/index.html"
+  }
+];
